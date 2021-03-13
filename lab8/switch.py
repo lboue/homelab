0c7578bb826256ba901608a73cfeeb84f8c53d8b
Tom <64171394+kd9cpb@users.noreply.github.com>
2021-03-13 17:57:54 -0600

    network automation with 8th grade python

--- lab8/switch.py
+++ lab8/switch.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+
 from netmiko import Netmiko
 import sys
 
+# redirect stdout to file for Netmiko to use later
 orig_stdout = sys.stdout
 sys.stdout = open("switchfile", "w")
 
+# generate the 150 switch vlan configs
 for i in range(100,150):
     print("vlan " + str(i))
     print("name wireless_user-v"+ str(i))
@@ -17,9 +21,11 @@ for i in range(200,250):
     print("name wired_user-v"+ str(i))
     print("tagged 3")
 
+# close the file, stop redirecting stdout 
 sys.stdout.close()
 sys.stdout = orig_stdout
 
+# setup Netmiko
 switch = {
             'host': '10.139.146.3',
             'username': 'tcostello',
@@ -29,6 +35,7 @@ switch = {
 switchconnect = Netmiko(**switch)
 switchconnect.enable()
 
+# run Netmiko, print what happens on switch, disconnect
 output = switchconnect.send_config_from_file("switchfile")
 print(output)
 switchconnect.disconnect()
