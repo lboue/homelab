a4f8ee580ffa5a29d8977a7c6c22d523a1d759d3
Tom <64171394+kd9cpb@users.noreply.github.com>
2021-06-19 23:09:11 -0500

    upload all scripts, configs, etc

---
+++ lab11/testcommit.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from netmiko import Netmiko
+
+# setup Netmiko
+nxosvars = {
+            'host': '10.140.2.3',
+            'username': 'netmiko',
+            'password': 'Netmiko1!',
+            'device_type': 'cisco_nxos',
+            }
+nxosconnect = Netmiko(**nxosvars)
+
+# get commands together
+nxoscommands = """
+config dual-stage
+int eth 1/8
+desc hello_from_netmiko
+commit confirmed 300
+"""
+
+# send commands into nxos
+output = nxosconnect.send_command(nxoscommands, cmd_verify=False)
+print(output)
+
+# use two-step config confirm
+commitanswer = input("Did the change work as planned? (y/n)")
+if commitanswer == "y":
+	output = nxosconnect.send_command("commit")
+	print(output)
+if commitanswer == "n":
+	output = nxosconnect.send_command("abort", expect_string=r"#")
+	print(output)
+
+nxosconnect.disconnect()
